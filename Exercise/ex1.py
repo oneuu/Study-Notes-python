4ed71f488d4fca719b0f819e96e1cb030bbc1bef
oneuu <36928414+oneuu@users.noreply.github.com>
2018-07-15 15:22:15 +0800

    Add files via upload

---
+++ Exercise/ex1.py
@@ -0,0 +1,9 @@
+print "hello world!"
+print "hello again..."
+print "i like typing this"
+print "this is fun"
+print "yay!Printing."
+print "i\'d much rather you 'not'."
+print "i \"said\" do not touch this..."
+print "add another line here 2018.05.06"
+# here is comment
